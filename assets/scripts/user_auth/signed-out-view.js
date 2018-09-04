0d0ec97fa2c10b01b062ae7d911df2240b65c572
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-09-04 09:25:36 -0400

    Clear Tool List
    - Add jQuery to 'signed-out-view.js' to clear tool list on sign out

--- assets/scripts/user_auth/signed-out-view.js
+++ assets/scripts/user_auth/signed-out-view.js
@@ -3,6 +3,7 @@
 const signOutView = function () {
   $('#change-password-modal-button, #sign-out, #get-tools, #see-one-tool, #add-a-tool, #edit-a-tool').hide()
   $('#sign-in-modal-button, #sign-up-modal-button').show()
+  $('#tool-list').html('')
 }
 
 module.exports = {
