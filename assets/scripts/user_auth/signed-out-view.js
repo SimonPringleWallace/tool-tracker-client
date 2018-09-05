340524c856501cc672333594f19632ec57b29f1e
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-09-05 17:14:35 -0400

    Refactor Sign Up/In
    - Refactor sign-in-view.js and signed-out-view.js so that they hide
      the overall class of 'signin-out buttons' rather than hiding the three
      elements individually
    - Add icon to index.html

--- assets/scripts/user_auth/signed-out-view.js
+++ assets/scripts/user_auth/signed-out-view.js
@@ -2,7 +2,7 @@
 
 const signOutView = function () {
   $('#change-password-modal-button, #sign-out, #get-tools, #see-one-tool, #add-a-tool, #edit-a-tool').hide()
-  $('#sign-in-modal-button, #sign-up-modal-button').show()
+  $('.signup-in-buttons').show()
   $('#tool-list, #message-box').html('')
 }
 
