e89c6d595da6dc7ae4fd3a6a75c3b0ab8cc5d0ae
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-09-05 12:06:55 -0400

    Edit file path in index.scss - edit file to make it accessible to webpack.

--- assets/scripts/user_auth/signed-in-view.js
+++ assets/scripts/user_auth/signed-in-view.js
@@ -3,7 +3,7 @@ const store = require('../store.js')
 
 const signInProcesses = function (serverResponse) {
   $('#change-password-modal-button, #sign-out, #get-tools, #see-one-tool, #add-a-tool, #edit-a-tool').show()
-  $('#sign-in-modal-button, #sign-up-modal-button, .message-box').hide()
+  $('#sign-in-modal-button, #sign-up-modal-button, #message-box').hide()
   store.user = serverResponse.user
 }
 module.exports = {
