9671d72df733d4352be12ffe40bf166faa529702
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-09-09 17:51:04 -0400

    Complete Hammer Animation
    - Add new hammer image to 'public/'
    - Add logic in 'hammer-animation-logic.js' to make the hammer strike on
      page load, but only float up and down if the user signs out and then
      signs in again.
    - Streamline signed-in-view.js and signed-out-view.js now that there
      is 'hammer-animation-logic.js' to do some of that lifting.

--- assets/scripts/user_auth/signed-in-view.js
+++ assets/scripts/user_auth/signed-in-view.js
@@ -1,10 +1,12 @@
 'use strict'
 const store = require('../store.js')
+const hammer = require('./hammer-animation-logic.js')
 
 const signInProcesses = function (serverResponse) {
   $('#change-password-modal-button, .launch-new-tool, #sign-out, #get-tools, #see-one-tool, #edit-a-tool, #create-tool-button').show()
   $('.signup-in-buttons').hide()
   $('#message-box').html('')
+  hammer.hammerAnimation()
   store.user = serverResponse.user
 }
 module.exports = {
--- assets/scripts/user_auth/signed-out-view.js
+++ assets/scripts/user_auth/signed-out-view.js
@@ -3,6 +3,7 @@
 const signOutView = function () {
   $('#change-password-modal-button, #sign-out, #get-tools, #see-one-tool, #add-a-tool, #edit-a-tool, #create-tool-button').hide()
   $('.signup-in-buttons').show()
+  $('.hammer-float').toggleClass('hammer-return')
   $('#tool-list, #message-box').html('')
 }
 
