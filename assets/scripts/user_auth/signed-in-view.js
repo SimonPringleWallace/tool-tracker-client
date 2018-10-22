2b9039f2c38272ba37b88ab1593b7f92c1ccf065
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-10-22 14:12:06 -0400

    Reset Branch for Hammer Animations
    - Reset branch back three commits. Nothing was accomplished in those
      other than a failed card-flip effect and it was holding back other
      improvements.

--- assets/scripts/user_auth/signed-in-view.js
+++ assets/scripts/user_auth/signed-in-view.js
@@ -3,7 +3,7 @@ const store = require('../store.js')
 const hammer = require('./hammer-animation-logic.js')
 
 const signInProcesses = function (serverResponse) {
-  $('#change-password-modal-button, .launch-new-tool, #sign-out, #get-tools, #see-one-tool, #edit-a-tool, #create-tool-button').fadeIn(1000)
+  $('#change-password-modal-button, .launch-new-tool, #sign-out, #get-tools, #see-one-tool, #edit-a-tool, #create-tool-button, #add-a-tool').fadeIn(1000)
   $('.signup-in-buttons').fadeOut(100)
   $('#message-box').html('')
   hammer.hammerAnimation()
